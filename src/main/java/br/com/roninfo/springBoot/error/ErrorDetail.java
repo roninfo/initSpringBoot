66365e475811dff2549ec99a90ddf8734d8bac19
Roni Palacio <roninfo@gmail.com>
2018-12-18 03:17:58 -0200

    interceptacao de exceptions

--- src/main/java/br/com/roninfo/springBoot/error/ErrorDetail.java
+++ src/main/java/br/com/roninfo/springBoot/error/ErrorDetail.java
@@ -7,6 +7,8 @@ public class ErrorDetail {
     private long timestamp;
     private String developerMessage;
 
+
+
     public String getTitle() {
         return title;
     }
@@ -45,5 +47,55 @@ public class ErrorDetail {
 
     public void setDeveloperMessage(String developerMessage) {
         this.developerMessage = developerMessage;
+    }
+
+    public static final class Builder {
+        private String title;
+        private int status;
+        private String detail;
+        private long timestamp;
+        private String developerMessage;
+
+        private Builder() {
+        }
+
+        public static Builder newBuilder() {
+            return new Builder();
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder developerMessage(String developerMessage) {
+            this.developerMessage = developerMessage;
+            return this;
+        }
+
+        public ErrorDetail build() {
+            ErrorDetail errorDetail = new ErrorDetail();
+            errorDetail.setTitle(title);
+            errorDetail.setStatus(status);
+            errorDetail.setDetail(detail);
+            errorDetail.setTimestamp(timestamp);
+            errorDetail.setDeveloperMessage(developerMessage);
+            return errorDetail;
+        }
     }
 }
--- src/main/java/br/com/roninfo/springBoot/handler/RestExceptionHandler.java
+++ src/main/java/br/com/roninfo/springBoot/handler/RestExceptionHandler.java
@@ -1,21 +1,27 @@
 package br.com.roninfo.springBoot.handler;
 
+import br.com.roninfo.springBoot.error.ErrorDetail;
 import br.com.roninfo.springBoot.error.ResourceNotFoundDetails;
 import br.com.roninfo.springBoot.error.ResourceNotFoundException;
 import br.com.roninfo.springBoot.error.ValidationErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundExcpetion(ResourceNotFoundException rnfException) {
@@ -31,10 +37,32 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(notFoundDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    // substituido pela sobrescrita do metodo
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+//
+//        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+//        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+//        String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+//
+//        ValidationErrorDetails notFoundDetails = ValidationErrorDetails.Builder
+//                .newBuilder()
+//                .timestamp(new Date().getTime())
+//                .status(HttpStatus.BAD_REQUEST.value())
+//                .title("Field Validation Error")
+//                .detail("Field Validation Error")
+//                .developerMessage(exception.getClass().getName())
+//                .field(fields)
+//                .fieldMessage(fieldMessages)
+//                .build();
+//
+//        return new ResponseEntity<>(notFoundDetails, HttpStatus.BAD_REQUEST);
+//    }
 
-        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
         String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
         String fieldMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
 
@@ -44,11 +72,40 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .title("Field Validation Error")
                 .detail("Field Validation Error")
-                .developerMessage(exception.getClass().getName())
+                .developerMessage(ex.getClass().getName())
                 .field(fields)
                 .fieldMessage(fieldMessages)
                 .build();
 
         return new ResponseEntity<>(notFoundDetails, HttpStatus.BAD_REQUEST);
+    }
+//      Substituido pelo override de handleExceptionInternal
+//    @Override
+//    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        ErrorDetail notFoundDetails = ErrorDetail.Builder
+//                .newBuilder()
+//                .timestamp(new Date().getTime())
+//                .status(HttpStatus.BAD_REQUEST.value())
+//                .title("Resource not found")
+//                .detail(ex.getMessage())
+//                .developerMessage(ex.getClass().getName())
+//                .build();
+//
+//        return new ResponseEntity<>(notFoundDetails, HttpStatus.BAD_REQUEST);
+//    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetails = ErrorDetail.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(status.value())
+                .title("Internal exception")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+
+        return new ResponseEntity(errorDetails, headers, status);
     }
 }
